33fdb3bae7346ae4658f87b199daa05ab7ead817
sagar-webonise <sagar.shirsath24@gmail.com>
2012-06-18 16:48:18 +0530

    spyke1 half done

--- spyke1/stringOp.php
+++ spyke1/stringOp.php
@@ -24,7 +24,76 @@ function allPosPHP($string)
     
 }
 
+function capstr($string)
+{
+  return strtoupper($string);
+}
 
+function combine($string1 , $string2)
+{
+    
+    return $string1.$string2;
+}
+
+function echoHeredoc($string1,$string2)
+{
+ /*$string1 = <<<'ONE'
+    PHP parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as PHP code. 
+    The parser then executes all of the code it finds until it runs into a PHP closing <br/> tag.
+     ONE;
+ $string2 = <<<'TWO'
+    PHP does not require (or support) explicit type definition in variable declaration; 
+    a variable's type is determined by the context in which the variable is used.
+    TWO;
+ */
+ return "string1="."'".$string1."<br/> string2="."'".$string2."'";
+}  
+
+function today()
+{
+    return date("Y/m/d");
+    
+}
+
+function dateform()
+{
+    $date = new DateTime('1/12/2012');
+    return $date->format('jS M Y');
+    
+}
+
+function sevenDays()
+{
+    $date = strtotime("+7 day", strtotime(date("Y/m/d")));
+    return date("Y/m/d", $date);
+    
+}
+
+function arrayOfWords()
+{
+    
+    $array = explode(" ",$string); 
+    
+}
+function spliteFour($string)
+{
+    $len = strlen($string);
+    $parts = str_split($string, 57);
+    $i = 0;
+    foreach($parts as $item)
+    {
+        echo ++$i.":  ";
+        echo $item."<br>";
+    }
+
+    
+}
+
+function divideAndComb($string)
+{
+    $array = explode(".",$string); 
+    
+}
 ?>
 <table border="1px">
        <tr><td >String1 = "<?php echo $string1; ?>"<td rowspan="1">String2="<?php echo $string1; ?>"</td></tr>
@@ -35,6 +104,31 @@ function allPosPHP($string)
     <tr><th>Questions</th><th>Answers</th></tr>
     <tr><td>1. Count occurance of PHP from string 1.</td>  <td><?php echo allOccPHP($string1);?></td></tr>
     <tr><td>2. Find the position where PHP occures in the string 1.</td><td><?php echo allPosPHP($string1);?></td></tr>
+    <tr><td>3. Create array of words in string 1 & print them using a recursive function.</td><td><?php arrayOfWords($string1);?></td></tr>
+    <tr><td>4. Capitalise string 1</td><td><?php echo capstr($string1);?></td></tr>
+    <tr><td>5. Combine string 1 & 2.</td><td><?php echo combine($string1 , $string2);?></td></tr>
+    <tr><td>6. Echo string 1 & 2 using heredoc.</td><td><?php echo echoHeredoc($string1,$string2);?></td></tr>
+    <tr><td>7. Print current date</td><td><?php echo today(); ?></td></tr>
+    <tr><td>8. print 12th Jan 2012</td><td><?php echo dateform(); ?></td></tr>
+    <tr><td>9. add 7 days in current date</td><td><?php echo sevenDays() ?></td></tr>
+    <tr><td>10. Cut the string 1 into 4 parts & print it.</td><td><?php spliteFour($string1) ?></td></tr>
+    <tr><td>11. Divide the string 1 by occurances of '.'. Combine the array in reverse word sequence</td><td><?php divideAndComb($string1) ?></td></tr>
+    <tr><td>12. Remove the HTML characters from string.</td><td></td></tr>
+    <tr><td>13. Print the 'PHP' word from string 1 by traversing it using string functions</td><td></td></tr>
+    <tr><td>14. Find the length of string 1 & 2</td><td></td></tr>
+    <tr><td>15. Create file & write string 1 to that file using PHP functions.</td><td></td></tr>
+    <tr><td>16. Print all Global varibles provided by PHP</td><td></td></tr>
+    <tr><td>17. Usage and examples of Header (PHP)</td><td></td></tr>
+    <tr><td>18. Redirect page 1 to page 2.</td><td></td></tr>
+    <tr><td>19. Compare two dates. (12-4-2010 & 12-5-2011). Calculate the days between these two dates.</td><td></td></tr>
+    <tr><td>20. Print date after 20 days from current date</td><td></td></tr>
+    <tr><td>21. Print date in array format.</td><td></td></tr>
+   
+        
+    
+    
+
+    
 </table>
 
 
